86c0c1db8bec1b4da10c15ca944d45d1476ee3ac
AJAY KUMAR <48375037+AJAYKR00KJ@users.noreply.github.com>
2021-04-08 18:23:52 +0530

    added navbar

---
+++ header.php
@@ -0,0 +1,26 @@
+<!-- navbar --> 
+
+<link rel="stylesheet" type="text/css" href="assets/css/navbar.css">
+
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+      <button style="width:200px;height:55px;" href="index.php" class="btn-hover color-1">SPARKS BANK</button>  
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <button href="index.php" class="btn-hover color-8">Home</button> 
+              </li>
+              <li class="nav-item">
+                <button href="#" class="btn-hover color-8">Create User</button> 
+              </li>
+              <li class="nav-item">
+                <button href="#" class="btn-hover color-8">Transfer Money</button> 
+              </li>
+              <li class="nav-item">
+                <button style="width:200px;" href="#" class="btn-hover color-8">Transaction History</button> 
+              </li>
+               
+          </div>
+       </nav>
